fe639f1321d856f1fc4c76c76d0fc5097bdbbb2d
Сорок два <sorokdva.developer@gmail.com>
2023-07-12 01:42:11 +0200

    fix: add missing user-agent header

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const main = (input) => {
   headers.append('content-length', '0')
   headers.append('origin', 'https://discord.com')
   headers.append('sec-ch-ua', '"Not?A_Brand";v="8", "Chromium";v="108"')
+  headers.append('user-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9013 Chrome/108.0.5359.215 Electron/22.3.2 Safari/537.36')
 
   const requestOptions = {
     method: 'POST',
